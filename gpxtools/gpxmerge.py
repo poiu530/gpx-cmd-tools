8fb5de2f50a6a56cce2239317fbfc74f05ed78c4
Paweł Kruszec <pakruszec@gmail.com>
2023-11-22 19:05:37 +0100

    Support glob patterns in gpxmerge

--- gpxtools/gpxmerge.py
+++ gpxtools/gpxmerge.py
@@ -4,6 +4,7 @@ import gpxpy
 import datetime
 import os.path as path
 import datetime
+import glob
 
 from . import common
 
@@ -19,16 +20,22 @@ def get_time(g: gpx.GPX) -> Optional[datetime.datetime]:
 
 def main() -> None:
     parser = argparse.ArgumentParser(description='Merge GPX files')
-    parser.add_argument('gpx_files', metavar='gpx', type=str, default='', nargs='*', help='GPX file')
+    parser.add_argument('gpx_patterns', metavar='gpx', type=str, default='', nargs='*', help='GPX files/patterns')
     parser.add_argument('-o', '--output', metavar='F', type=str, default='merged.gpx', help='Output GPX file')
     parser.add_argument('-t', '--time', action='store_true', help='Sort by time')
     args = parser.parse_args()
 
-    gpx_files = args.gpx_files
+    gpx_patterns = args.gpx_patterns
     out_file = args.output
     sort_by_time = args.time
 
     keep_extensions = True
+
+    gpx_files = []
+    for gpx_pattern in gpx_patterns:
+        for gpx_file in glob.glob(gpx_pattern):
+            if gpx_file not in gpx_files:
+                gpx_files.append(gpx_file)
 
     if not gpx_files:
         print("Nothing to do")
